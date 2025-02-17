1b1246ecdfcfbbe59f83e60d9af56da0ef69eeb6
waynelloydsmith <37414657+waynelloydsmith@users.noreply.github.com>
2025-02-17 12:55:04 -0800

    Add files via upload

---
+++ AccountNames.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf8
+# version 2/17/2025
+
+
+global accountNames
+# Scotia Bank
+accountNames = ['BMO RRSP TEST',
+              'Scotia1',
+	      'Scotia2'
+              ]
+
+global BMOAccounts
+BMOAccounts = ['BMO RRSP TEST']
---
+++ Passwords.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+global UserNamme
+UserName = "waynelloydsmith"
+global Password
+PassWord = "Escape2001"
+
